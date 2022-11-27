67a10495d14cb74f0df2e315e4dd4e027d490f7f
Girish keleng <girishkeleng30@gmail.com>
2022-11-27 18:19:29 +0530

    ADD: redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,193 @@
-import React from 'react'
+import React,{useState} from "react";
+import styled from "styled-components";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { Translate } from "@material-ui/icons";
 
 const Header = () => {
+ 
+  const [burgerStatus,setBurgerStatus] = useState(false);
+
   return (
-    <div>header</div>
-  )
+    <Container>
+      <a href="https://www.tesla.com/">
+        <img src="/images/logo.svg" alt="" />
+      </a>
+
+      <Menu className="menu">
+        <a href="https://www.tesla.com/">Model S</a>
+        <a href="https://www.tesla.com/">Model 3</a>
+        <a href="https://www.tesla.com/">Model X</a>
+        <a href="https://www.tesla.com/">Model Y</a>
+        <a href="https://www.tesla.com/">Solar Panels</a>
+        <a href="https://www.tesla.com/">Solar Roof</a>
+      </Menu>
+
+      <RightMenu>
+        <InsideRightMenu>
+          <a href="https://www.tesla.com/">Shop</a>
+          <a href="https://www.tesla.com/">Account</a>
+        </InsideRightMenu>
+      
+        {/* <CustomMenu onClick={()=> setBurgerStatus(true)}/> */}
+        <CustomMenu >
+          <a onClick={()=> setBurgerStatus(true)}>Menu</a>
+        </CustomMenu>
+
+      </RightMenu>
+
+        <BurgerNav show={burgerStatus} >
+
+          <CloseWrapper>
+
+            <CustomClose onClick={()=> setBurgerStatus(false)} />
+
+          </CloseWrapper>
+          
+          <li><a href="https://www.tesla.com/">Model S</a></li>
+          <li><a href="https://www.tesla.com/">Model 3</a></li>
+          <li><a href="https://www.tesla.com/">Model X</a></li>
+          <li><a href="https://www.tesla.com/">Model Y</a></li>
+          <li><a href="https://www.tesla.com/">Solar Roof</a></li>
+          <li><a href="https://www.tesla.com/">Solar Panels</a></li>
+          <li><a href="https://www.tesla.com/">Existing inventory</a></li>
+          <li><a href="https://www.tesla.com/">Used inventory</a></li>
+          <li><a href="https://www.tesla.com/">Trad-In</a></li>
+          <li><a href="https://www.tesla.com/">Test Drive</a></li>
+          <li><a href="https://www.tesla.com/">Insurance</a></li>
+          <li><a href="https://www.tesla.com/">Powerwall</a></li>
+          <li><a href="https://www.tesla.com/">Powerwall</a></li>
+        </BurgerNav>
+
+
+
+
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
+  z-index:1;
+`;
+
+const Menu = styled.div`
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    padding: 0 10px;
+  }
+
+  @media (max-width: 990px) {
+    display: none;
+  }
+`;
+const RightMenu = styled.div`
+
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    // padding: 0 10px;
+    margin: 0 20px ;
+    text-decoration: none;
+  }
+ 
+
+`;
+
+const InsideRightMenu = styled.div`
+  @media (max-width: 900px) {
+    a {
+      display: none;
+    }
+  }
+`;
+
+// const CustomMenu = styled(MenuIcon)`
+//   cursor: pointer;
+// `;
+const CustomMenu = styled.div`
+
+  cursor: pointer;
+  @media (max-width: 900px) {
+    
+    align-items: flex-end;
+   
+   a{ 
+     background:rgba(4,35, 103,0.1);
+    // background: #8888;
+    border-radius:3px;
+     padding:7px;
+    
+    opacity:0.9;
+  }
+}
+`;
+
+const BurgerNav = styled.div`
+
+position: fixed;
+right: 0;
+top: 0;
+bottom: 0;
+background-color: #fff;
+width: 300px;
+z-index:100;
+list-style: none;
+padding:20px;
+display: flex;
+justify-content:flex-start;
+flex-direction: column;
+align-items: flex-start;
+
+ transform: ${props => props.show ? 'TranslateX(0)': 'TranslateX(100%)'};
+
+ transition: transform 0.4s ease-in-out ;
+
+
+li{
+  padding:15px;
+  border-bottom: 1px solid #888;
+  // width: 100%;
 }
 
-export default Header
+a{
+  text-decoration: none;
+  font-weight: 600;
+}
+
+
+`
+
+const CustomClose = styled(CloseIcon)`
+cursor: pointer;
+
+`
+
+const CloseWrapper = styled.div`
+display:flex;
+justify-content:flex-end;
+width:100%;
+
+
+
+`
+const burgerStatus = styled.div`
+
+`
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,104 +1,114 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-const Section = ({ title,description,leftBtnText, rightBtnText,backgroundImg}) => {  // or you can pass only props and use {props.title} inside
-   
+import Fade from "react-reveal/Fade";
+
+const Section = ({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) => {
+  // or you can pass only props and use {props.title} inside
+
   return (
-    <Wrap bgImage ={backgroundImg} >
-        
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
         <ItemText>
-             <h1>{title}</h1>
-             <p>{description}</p>
+          <h1>{title}</h1>
+          <p>
+            <a href="#"> {description}</a>
+          </p>
         </ItemText>
+      </Fade>
 
-        <Buttons> 
-
+      <Buttons>
         <ButtonGroup>
-            <LeftButton>
-                {leftBtnText}
-            </LeftButton>
-
-            {rightBtnText && 
-               <RightButton> 
-                {rightBtnText}
-               </RightButton>
-            } 
-
-            
+          <Fade left>
+            <LeftButton>{leftBtnText}</LeftButton>
+          </Fade>
+          <Fade right>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </Fade>
         </ButtonGroup>
 
-       <DownArrow src="/images/down-arrow.svg" />
-</Buttons>
-       
-
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-width: 100vw;
-height: 100vh;
-background-size: cover;
-background-position: center;
-background-repeat: no-repeat;
-background-image: url('/images/model-3.jpg');
-display: flex;
-flex-direction: column;
-justify-content: space-between;  // Vertical alignment
-align-items: center;  // Horizontal alignment
-background-image: ${props => `url('/images/${props.bgImage}')`}
-
-`
+  position: static;
+  z-index: 10;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-3.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; // Vertical alignment
+  align-items: center; // Horizontal alignment
+  background-image: ${(props) => `url('/images/${props.bgImage}')`};
+  position: static;
+  z-index:1;
+`;
 
 const ItemText = styled.div`
-padding-top: 15vh;
-text-align: center;
 
-`
+  padding-top: 15vh;
+  text-align: center;
+  z-index: 10;
+
+  h1 {
+    font-size: 2.5rem;
+  }
+`;
+
 const ButtonGroup = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-margin-bottom: 2rem;
-cursor: pointer;
-@media (max-width: 768px){
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+  cursor: pointer;
+  @media (max-width: 768px) {
     flex-direction: column;
-}
-
-`
+  }
+`;
 const LeftButton = styled.div`
-background-color: rgba(23,26,32,0.8);
-height :40px;
-width: 256px;
-color: #fff;
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius:8px;
-opacity: 0.85;
-text-transform: uppercase;
-font-size:0.9rem;
-font-weight:500;
-margin:10px;
 
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 0.9rem;
+  font-weight: 600;
+  margin: 10px;
+`;
 
 const RightButton = styled(LeftButton)`
-opacity: 0.9;
-background-color:rgb(240, 240, 240);
-color:#888;
 
-
-`
+  opacity: 0.9;
+  background-color: rgb(240, 240, 240);
+  color: #888;
+`;
 const DownArrow = styled.img`
-margin-top: 20px;
-height:40px;
-animation: animateDown infinite 1.5s;
-overflow: hidden;
 
-`
-const Buttons= styled.div`
-
-`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow: hidden;
+`;
+const Buttons = styled.div``;
